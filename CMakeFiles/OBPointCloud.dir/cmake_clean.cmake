f0fdf0d0166073b743e7f9c2f4f3cfc4c7c30cbd
EnesUlasDincer <enes.ulas.dincer@gmail.com>
2025-01-10 17:38:00 +0100

    sdf

---
+++ CMakeFiles/OBPointCloud.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OBPointCloud.dir/src/PointCloud.cpp.o"
+  "CMakeFiles/OBPointCloud.dir/src/PointCloud.cpp.o.d"
+  "CMakeFiles/OBPointCloud.dir/src/funcs.cpp.o"
+  "CMakeFiles/OBPointCloud.dir/src/funcs.cpp.o.d"
+  "CMakeFiles/OBPointCloud.dir/src/pointCloud_utils.cpp.o"
+  "CMakeFiles/OBPointCloud.dir/src/pointCloud_utils.cpp.o.d"
+  "OBPointCloud"
+  "OBPointCloud.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OBPointCloud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
